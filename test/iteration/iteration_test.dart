b049c58fee00043829e926297d66f96da8e5135e
Jonas Wanke <contact@jonas-wanke.com>
2020-05-25 16:53:36 +0200

    test: add more recurrence tests

--- test/iteration/iteration_test.dart
+++ test/iteration/iteration_test.dart
@@ -15,10 +15,14 @@ void main() {
   }) {
     test(description, () {
       if (isInfinite) {
+        final actual =
+            rrule.getInstances(start: start).take(expectedDates.length * 2);
         expect(
-          rrule.getInstances(start: start).take(expectedDates.length),
-          expectedDates,
+          actual.length,
+          expectedDates.length * 2,
+          reason: 'Is actually \'infinite\'',
         );
+        expect(actual.take(expectedDates.length), expectedDates);
       } else {
         expect(rrule.getInstances(start: start), expectedDates);
       }
@@ -76,4 +80,117 @@ void main() {
       ...2.to(13, by: 10).map((d) => LocalDate(1997, 10, d)),
     ].map((d) => d.at(LocalTime(9, 0, 0))),
   );
+  group('Every day in January, for 3 years', () {
+    final expected = 1998.to(2001).expand((y) {
+      return 1.to(32).map((d) => LocalDateTime(y, 1, d, 9, 0, 0));
+    });
+    testRecurring(
+      'with frequency yearly',
+      rrule: RecurrenceRule(
+        frequency: RecurrenceFrequency.yearly,
+        until: LocalDateTime(2000, 01, 31, 14, 0, 0),
+        byWeekDays: {
+          ByWeekDayEntry(DayOfWeek.sunday),
+          ByWeekDayEntry(DayOfWeek.monday),
+          ByWeekDayEntry(DayOfWeek.tuesday),
+          ByWeekDayEntry(DayOfWeek.wednesday),
+          ByWeekDayEntry(DayOfWeek.thursday),
+          ByWeekDayEntry(DayOfWeek.friday),
+          ByWeekDayEntry(DayOfWeek.saturday),
+        },
+        byMonths: {1},
+      ),
+      start: LocalDateTime(1998, 1, 1, 9, 0, 0),
+      expectedDates: expected,
+    );
+    testRecurring(
+      'with frequency daily',
+      rrule: RecurrenceRule(
+        frequency: RecurrenceFrequency.daily,
+        until: LocalDateTime(2000, 01, 31, 14, 0, 0),
+        byMonths: {1},
+      ),
+      start: LocalDateTime(1998, 1, 1, 9, 0, 0),
+      expectedDates: expected,
+    );
+  });
+  testRecurring(
+    'Weekly for 10 occurrences',
+    rrule: RecurrenceRule(
+      frequency: RecurrenceFrequency.weekly,
+      count: 10,
+    ),
+    start: LocalDateTime(1997, 9, 2, 9, 0, 0),
+    expectedDates: [
+      ...2.to(31, by: 7).map((d) => LocalDate(1997, 9, d)),
+      ...7.to(29, by: 7).map((d) => LocalDate(1997, 10, d)),
+      LocalDate(1997, 11, 4),
+    ].map((d) => d.at(LocalTime(9, 0, 0))),
+  );
+  testRecurring(
+    'Weekly until December 24, 1997',
+    rrule: RecurrenceRule(
+      frequency: RecurrenceFrequency.weekly,
+      until: LocalDate(1997, 12, 24).atMidnight(),
+    ),
+    start: LocalDateTime(1997, 9, 2, 9, 0, 0),
+    expectedDates: [
+      ...2.to(31, by: 7).map((d) => LocalDate(1997, 9, d)),
+      ...7.to(29, by: 7).map((d) => LocalDate(1997, 10, d)),
+      ...4.to(26, by: 7).map((d) => LocalDate(1997, 11, d)),
+      ...2.to(24, by: 7).map((d) => LocalDate(1997, 12, d)),
+    ].map((d) => d.at(LocalTime(9, 0, 0))),
+  );
+  testRecurring(
+    'Every other week - forever',
+    rrule: RecurrenceRule(
+      frequency: RecurrenceFrequency.weekly,
+      interval: 2,
+      weekStart: DayOfWeek.sunday,
+    ),
+    start: LocalDateTime(1997, 9, 2, 9, 0, 0),
+    expectedDates: [
+      ...2.to(31, by: 14).map((d) => LocalDate(1997, 9, d)),
+      ...14.to(29, by: 14).map((d) => LocalDate(1997, 10, d)),
+      ...11.to(26, by: 14).map((d) => LocalDate(1997, 11, d)),
+      ...9.to(24, by: 14).map((d) => LocalDate(1997, 12, d)),
+      ...6.to(21, by: 14).map((d) => LocalDate(1998, 1, d)),
+      ...3.to(18, by: 14).map((d) => LocalDate(1998, 2, d)),
+    ].map((d) => d.at(LocalTime(9, 0, 0))),
+    isInfinite: true,
+  );
+  group('Weekly on Tuesday and Thursday for five weeks', () {
+    final expected = [
+      ...[2, 4, 9, 11, 16, 18, 23, 25, 30].map((d) => LocalDate(1997, 09, d)),
+      LocalDate(1997, 10, 2),
+    ].map((d) => d.at(LocalTime(9, 0, 0)));
+    testRecurring(
+      'with until',
+      rrule: RecurrenceRule(
+        frequency: RecurrenceFrequency.weekly,
+        until: LocalDate(1997, 10, 07).atMidnight(),
+        weekStart: DayOfWeek.sunday,
+        byWeekDays: {
+          ByWeekDayEntry(DayOfWeek.tuesday),
+          ByWeekDayEntry(DayOfWeek.thursday),
+        },
+      ),
+      start: LocalDateTime(1997, 9, 2, 9, 0, 0),
+      expectedDates: expected,
+    );
+    testRecurring(
+      'with count',
+      rrule: RecurrenceRule(
+        frequency: RecurrenceFrequency.weekly,
+        count: 10,
+        byWeekDays: {
+          ByWeekDayEntry(DayOfWeek.tuesday),
+          ByWeekDayEntry(DayOfWeek.thursday),
+        },
+        weekStart: DayOfWeek.sunday,
+      ),
+      start: LocalDateTime(1997, 9, 2, 9, 0, 0),
+      expectedDates: expected,
+    );
+  });
 }
