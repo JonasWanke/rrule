a96de01b8cbed4fb625190d4c0e504b6f81420f9
Jonas Wanke <contact@jonas-wanke.com>
2020-05-26 13:49:49 +0200

    refactor: move recurrence tests from iCal to separate file

--- test/iteration/iteration_ical_test.dart
+++ test/iteration/iteration_ical_test.dart
@@ -4,38 +4,11 @@ import 'package:rrule/rrule.dart';
 import 'package:test/test.dart';
 import 'package:time_machine/time_machine.dart';
 
+import 'utils.dart';
+
 void main() {
-  @isTest
-  void testRecurring(
-    String description, {
-    @required RecurrenceRule rrule,
-    @required LocalDateTime start,
-    Iterable<LocalDate> expectedDates,
-    Iterable<LocalDateTime> expectedDateTimes,
-    bool isInfinite = false,
-  }) {
-    assert((expectedDates == null) != (expectedDateTimes == null));
-
-    test(description, () {
-      final expected = expectedDateTimes ??
-          expectedDates.map((d) => d.at(LocalTime(9, 0, 0)));
-
-      if (isInfinite) {
-        final actual =
-            rrule.getInstances(start: start).take(expected.length * 2);
-        expect(
-          actual.length,
-          expected.length * 2,
-          reason: 'Is actually \'infinite\'',
-        );
-        expect(actual.take(expected.length), expected);
-      } else {
-        expect(rrule.getInstances(start: start), expected);
-      }
-    });
-  }
-
-  // All examples taken from https://tools.ietf.org/html/rfc5545#section-3.8.5.3.
+  // This file tests all examples given in RFC 5545:
+  // https://tools.ietf.org/html/rfc5545#section-3.8.5.3.
   testRecurring(
     'Daily for 10 occurrences',
     rrule: RecurrenceRule(
